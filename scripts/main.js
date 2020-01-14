eb2ab3312e76bd3ad729c30c2b012b9dede057c8
rogerwmwong <rogerwmwong@gmail.com>
2020-01-15 00:56:11 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem('name', myName);
-      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+      myHeading.innerHTML = 'Click the picture, ' + myName;
     }
 }
 
